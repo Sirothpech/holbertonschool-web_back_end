fd4fd9dc623ee374cb11c6bf8763a3b74421cee0
Sirothpech <christophe.ngan@gmail.com>
2023-08-29 16:30:01 +0200

    Fix the pep8 add the docstring

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""
-This module contains a function that adds two floats.
-"""
+"""This module contains a function that adds two floats."""
 
 
 def add(a: float, b: float) -> float:
     """
     Returns the sum of two floats as a float.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of the two input numbers as a float.
     """
     return a + b
