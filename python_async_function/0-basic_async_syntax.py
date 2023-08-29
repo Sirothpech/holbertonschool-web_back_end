677b7ab19b20b6c26f13a9d301047729cd591769
Sirothpech <christophe.ngan@gmail.com>
2023-08-30 00:36:35 +0200

    Fix the pep8 docstring

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
 """
+Coroutine that takes in an integer argument that waits for a random delay
 """
 import asyncio
 import random
 
-
 async def wait_random(max_delay: int = 10) -> float:
-    """[summary]
+    """
+    Asynchronous function that generates a random delay and waits
+    for that amount of time before returning a float value.
 
     Args:
-        max_delay (int, optional): [description]. Defaults to 10.
+        max_delay (int, optional): The maximum delay in seconds.
+        Defaults to 10.
 
     Returns:
-        float: [description]
+        float: A float value representing the delay.
     """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
