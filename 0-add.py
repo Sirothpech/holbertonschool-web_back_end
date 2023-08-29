63c174236168256877a0e3d75fcd8ccb4fe71764
Sirothpech <christophe.ngan@gmail.com>
2023-08-29 16:13:55 +0200

    Add: the readmefile and the script to typing the method add

---
+++ 0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains a function that adds two floats.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Returns the sum of two floats as a float.
+    """
+    return a + b
