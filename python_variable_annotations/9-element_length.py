24e4c89a9b3359e899fe4ff8381e7337e409c801
Sirothpech <christophe.ngan@gmail.com>
2023-08-29 23:33:54 +0200

    Fix the pep8, use the docstrings

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -7,4 +7,15 @@ from typing import Iterable, Sequence, Tuple, List
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Returns a list of tuples, where each tuple contains a sequence
+    from the input list and its corresponding length.
+
+    Args:
+        lst (Iterable[Sequence]): A list of sequences.
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples, where each tuple
+        contains a sequence from the input list and its corresponding length.
+    """
     return [(i, len(i)) for i in lst]
