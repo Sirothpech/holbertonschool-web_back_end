e1bea9af5ef355a596420f2847637c12f0a26707
Sirothpech <christophe.ngan@gmail.com>
2023-08-29 17:08:19 +0200

    Add: the typing to the method  returns the string representation of the float.

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!usr/bin/env python3
+"""This module contians a function that returns the string
+representation of a float."""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
