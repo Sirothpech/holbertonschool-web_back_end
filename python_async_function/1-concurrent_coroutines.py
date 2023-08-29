2bc358e28c13865f70abb750ee379d8185236741
Sirothpech <christophe.ngan@gmail.com>
2023-08-30 00:53:59 +0200

    Add: method async takes an int and return list of values for a random delay

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Coroutine that takes in an integer argument that waits for a random delay
+"""
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """
+    Asynchronous function that takes in an integer argument that waits
+    for a random delay and then returns a list of all the delays.
+
+    Args:
+        n (int, optional): The number of times wait_random() will be called.
+
+        max_delay (int, optional): The maximum delay in seconds.
+
+    Returns:
+        float: A list of all the delays.
+    """
+    delays = []
+    for i in range(n):
+        delays.append(await wait_random(max_delay))
+    return sorted(delays)
