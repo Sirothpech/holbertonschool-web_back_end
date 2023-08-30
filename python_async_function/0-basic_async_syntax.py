d71a493b9d0a47e7f4d7ad0e133675c362aac3ff
Sirothpech <christophe.ngan@gmail.com>
2023-08-30 13:05:00 +0200

    Fix the pep8

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,7 @@ Coroutine that takes in an integer argument that waits for a random delay
 import asyncio
 import random
 
+
 async def wait_random(max_delay: int = 10) -> float:
     """
     Asynchronous function that generates a random delay and waits
