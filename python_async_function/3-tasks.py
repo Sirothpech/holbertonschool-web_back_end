61072d321fb4b9d0ca7a9a2e23cb4f51e9851bbd
Sirothpech <christophe.ngan@gmail.com>
2023-08-30 10:51:20 +0200

    Add: Coroutine that takes in an integer argument on the asyncio task.

---
+++ python_async_function/3-tasks.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Coroutine that takes in an integer argument on the asyncio task.
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """[summary]
+
+    Args:
+        max_delay (int): [description]
+
+    Returns:
+        asyncio.Task: [description]
+    """
+    return asyncio.create_task(wait_random(max_delay))
