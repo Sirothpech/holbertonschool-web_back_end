35502b482693c447770dca8ce331e283cb4f897d
Sirothpech <christophe.ngan@gmail.com>
2023-09-05 19:25:26 +0200

    Add: task 11

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Script lists all documents in a collection
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    List all documents in Python
+    """
+    if mongo_collection is None:
+        return []
+    return mongo_collection.find({"topics": topic})
