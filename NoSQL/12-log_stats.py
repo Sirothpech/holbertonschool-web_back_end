6139ce1afbc9e0c3b6798785c4e736b521645dd8
Sirothpech <christophe.ngan@gmail.com>
2023-09-05 19:37:40 +0200

    CHMOD

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -24,5 +24,3 @@ if __name__ == "__main__":
         nginx_collection.count_documents({"$and": [{"path": "/status"},
                                                    {"method": "GET"}]})
     ))
-
-
