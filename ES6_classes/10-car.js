058b1962c2c2acb65f5a693f338b3f19b92c2323
Sirothpech <christophe.ngan@gmail.com>
2023-09-15 20:25:45 +0200

    Add: task 10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,35 @@
+export default class car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(newBrand) {
+    this._brand = newBrand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(newMotor) {
+    this._motor = newMotor;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(newColor) {
+    this._color = newColor;
+  }
+
+  cloneCar() {
+    return new car(this.brand, this.motor, this.color);
+  }
+}
