9ea43b04128bb7d6d8311c4b62f9b88aea86f1df
Sirothpech <christophe.ngan@gmail.com>
2023-10-03 15:53:22 +0200

    Fix the issue, don't use the status code explicitly

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,10 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
-
-  return Promise.allSettled(promises).then((results) => {
-    return results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return { status: 200, value: result.value };
-      } else {
-        return { status: 500, value: result.reason.message };
-      }
-    });
-  });
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : `${result.reason.name}: ${result.reason.message}`,
+    })));
 }
