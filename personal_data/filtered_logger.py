9569547823926f1fed19ad3795991192969cd5c7
Sirothpech <christophe.ngan@gmail.com>
2024-02-19 10:26:40 +0100

    Fix the annotations

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,9 +4,11 @@
 """
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
     Obfuscate sensitive information in a log message.
 
