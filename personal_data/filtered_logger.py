7c4a20556a0a2b06887bbee26b08e1d155322c71
Sirothpech <christophe.ngan@gmail.com>
2024-02-19 11:16:00 +0100

    Refactor RedactingFormatter class in filtered_logger.py for better readability and documentation

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -24,22 +24,49 @@ def filter_datum(fields: List[str], redaction: str,
     """
     pattern = "|".join(fields)
     regex = f"({pattern})=(.*?){separator}"
-    return re.sub(regex, f"\\1={redaction}{separator} ", message)
+    return re.sub(regex, f"\\1={redaction}{separator}", message)
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
+    """Custom log formatter for redacting sensitive information.
 
+    Attributes:
+        REDACTION (str): String to replace sensitive information.
+        FORMAT (str): Log format string.
+        SEPARATOR (str): Separator character in the log message.
+
+    Args:
+        fields (List[str]): List of fields to redact.
+
+    Methods:
+        format(record: LogRecord) -> str:
+            Format the log record, redacting sensitive information.
+
+    """
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str] = ()):
+        """Initialize the RedactingFormatter.
+
+        Args:
+            fields (List[str]): List of fields to redact.
+
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Format the log record, redacting sensitive information.
+
+        Args:
+            record (LogRecord): The log record to be formatted.
+
+        Returns:
+            str: The formatted log message.
+
+        """
         log_message = super().format(record)
         filtered_message = filter_datum(self.fields, self.REDACTION,
                                         log_message, self.SEPARATOR)
