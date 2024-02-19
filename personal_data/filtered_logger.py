8bc171e1103c6f580472ac68411981921ba44315
Sirothpech <christophe.ngan@gmail.com>
2024-02-19 10:54:22 +0100

    Update: Implement filter_datum function to obfuscate sensitive information in log messages. The function uses regular expressions to replace specified fields with a redacted value, and adds a space after each separator in the result.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """
-0. Regex-ing
+1. Log formatter
 """
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -23,4 +24,23 @@ def filter_datum(fields: List[str], redaction: str,
     """
     pattern = "|".join(fields)
     regex = f"({pattern})=(.*?){separator}"
-    return re.sub(regex, f"\\1={redaction}{separator}", message)
+    return re.sub(regex, f"\\1={redaction}{separator} ", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields=()):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        log_message = super().format(record)
+        filtered_message = filter_datum(self.fields, self.REDACTION,
+                                        log_message, self.SEPARATOR)
+        return filtered_message
