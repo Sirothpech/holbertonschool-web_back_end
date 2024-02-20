99c0193390cde45caa9b1a0498d17e448d4f3316
Sirothpech <christophe.ngan@gmail.com>
2024-02-20 13:38:34 +0100

    Implemented basic authentication in api/v1/app.py and updated methods in auth.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.views import app_views
+from api.v1.auth.auth import Auth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -13,6 +14,22 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+
+# Load the right instance of authentication based on AUTH_TYPE
+auth = Auth()
+
+def before_request():
+    """ Method to handle before_request
+    """
+    if auth.require_auth(request.path, ['/api/v1/status/', '/api/v1/unauthorized/',
+                                        '/api/v1/forbidden/']):
+        if auth.authorization_header(request) is None:
+            abort(401)
+
+        if auth.current_user(request) is None:
+            abort(403)
+
+app.before_request(before_request)
 
 @app.errorhandler(404)
 def not_found(error) -> str:
