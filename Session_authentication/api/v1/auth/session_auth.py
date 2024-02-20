20cde2f2a392cc3291342b59c831bf4ad1b82d24
Sirothpech <christophe.ngan@gmail.com>
2024-02-20 17:56:19 +0100

    Added session creation functionality to session_auth.py

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Empty session
 """
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -17,4 +18,15 @@ class SessionAuth(Auth):
     Attributes:
         (No additional attributes for now)
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        # Generate a Session ID using uuid4()
+        session_id = str(uuid.uuid4())
+
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
