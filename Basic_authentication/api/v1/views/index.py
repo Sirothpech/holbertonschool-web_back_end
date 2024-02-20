2c721a8b7ccbfa92f8b0cd691a92e77f27e32ad4
Sirothpech <christophe.ngan@gmail.com>
2024-02-20 13:20:49 +0100

    Refactor API endpoints and add return type hints in index.py

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -5,14 +5,14 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized_route():
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_route() -> str:
     """ Endpoint that raises a 401 error """
     abort(401)
 
 
-@app_views.route('/api/v1/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden_route():
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden_route() -> str:
     """ Endpoint that raises a 403 error """
     abort(403)
 
