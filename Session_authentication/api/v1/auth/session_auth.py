f25ade3c53cbc82c974a9cd5ed065c52f5c71785
Sirothpech <christophe.ngan@gmail.com>
2024-02-20 17:46:58 +0100

    Added SessionAuth class docstring in session_auth.py

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -5,5 +5,16 @@ from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
+    """
+    SessionAuth Class
 
+    This class inherits from the Auth class and is intended for creating a new
+    authentication mechanism based on sessions.
+
+    Methods:
+        (No additional methods for now)
+
+    Attributes:
+        (No additional attributes for now)
+    """
     pass
