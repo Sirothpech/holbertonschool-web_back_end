a5903b14a1464ea580ec31e4dd74f0cf79dff47a
Sirothpech <christophe.ngan@gmail.com>
2024-02-20 20:08:14 +0100

    Refactor before_request method in app.py for better readability and error handling

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -29,19 +29,24 @@ else:
 
 
 @app.before_request
-def before_request():
-    """ Method to handle before_request
+def before_request() -> None:
+    """ Before request
     """
-    if auth.require_auth(request.path, ['/api/v1/status/',
-                                        '/api/v1/unauthorized/',
-                                        '/api/v1/forbidden/',
-                                        '/api/v1/auth_session/login/']):
-        if auth.authorization_header(request) and\
-        auth.session_cookie(request) is None:
-            abort(401)
+    if auth is None:
+        return
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
+    if not auth.require_auth(request.path, excluded_paths):
+        return
 
-        if auth.current_user(request) is None:
-            abort(403)
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
+        abort(401)
+
     request.current_user = auth.current_user(request)
 
 
