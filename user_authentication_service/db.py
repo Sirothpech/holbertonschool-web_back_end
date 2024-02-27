fdeca4f4e80098b30aa90d5820da86d94c0126b0
Sirothpech <christophe.ngan@gmail.com>
2024-02-27 17:04:11 +0100

    Added update_user method in db.py for user attribute modification

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -52,3 +52,17 @@ class DB:
             return user
         except InvalidRequestError:
             raise
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update user into the DB
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+            for key, value in kwargs.items():
+                if hasattr(User, key):
+                    setattr(user, key, value)
+                else:
+                    raise ValueError(f"Invalid attribute: {key}")
+            self._session.commit()
+        except NoResultFound:
+            raise ValueError(f"No user found with id {user_id}")
