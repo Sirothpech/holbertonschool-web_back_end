e63c543f1b98dbdad29cb13e0aadf67c0850bc08
Sirothpech <christophe.ngan@gmail.com>
2024-02-28 20:00:55 +0100

    Added route for user registration in app.py

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -16,6 +16,7 @@ def hello() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', method=['POST'], strict_slashes=False)
 def reg_user() -> str:
     """Register user route
