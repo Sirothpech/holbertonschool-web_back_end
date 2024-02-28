f33323daeedccf6958f43458b740a9fd91779aef
Sirothpech <christophe.ngan@gmail.com>
2024-02-28 20:08:34 +0100

    Fixed typo in route decorator parameters in user_authentication_service/app.py

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -17,7 +17,7 @@ def hello() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', method=['POST'], strict_slashes=False)
+@app.route('/users', methods=['POST'], strict_slashes=False)
 def reg_user() -> str:
     """Register user route
     """
