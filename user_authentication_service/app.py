6592f8fbf42c889d36661c4c2f32325ca961997d
Sirothpech <christophe.ngan@gmail.com>
2024-02-28 19:52:49 +0100

    Added user registration route to Flask app in user_authentication_service

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,15 +2,33 @@
 """T6: create app flask
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+AUTH = Auth()
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello() -> str:
-    """Test route
+    """hello route
     """
     return jsonify({"message": "Bienvenue"})
+
+@app.route('/users', method=['POST'], strict_slashes=False)
+def reg_user() -> str:
+    """Register user route
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
+
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
