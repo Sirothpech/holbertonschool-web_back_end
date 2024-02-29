17bc3455ff0b20b0f6a2dd1e4b229a52df1cff17
Sirothpech <christophe.ngan@gmail.com>
2024-02-29 15:23:57 +0100

    Refactor auth.py: return user object after successful registration

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -40,7 +40,8 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            self._db.add_user(email, _hash_password(password))
+            user = self._db.add_user(email, _hash_password(password))
+            return user
 
     def valid_login(self, email: str, password: str) -> bool:
         """Validates login credentials
