a13992a48ce11f5c012ff1143119585f8fc2f0f6
Sirothpech <christophe.ngan@gmail.com>
2024-02-29 14:19:44 +0100

    Refactor create_session method in auth.py for clarity and error handling

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -55,12 +55,12 @@ class Auth:
             return False
 
     def create_session(self, email: str) -> str:
-        """Create session user
+        """ Creates a session for a user.
         """
         try:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
+            return session_id
         except NoResultFound:
-            return
-        return session_id
+            return None
