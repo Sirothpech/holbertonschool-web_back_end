7380b98d6111c40667a0d74f4af76149859aad34
Sirothpech <christophe.ngan@gmail.com>
2024-02-29 13:06:22 +0100

    Added login route and functionality in user_authentication_service/app.py

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """T6: create app flask
 """
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 
 AUTH = Auth()
@@ -30,6 +30,23 @@ def reg_user() -> str:
 
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Login user.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email,"message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
