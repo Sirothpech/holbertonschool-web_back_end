9cf0272f3dc1d1efab6165337172e534632cd94a
Sirothpech <christophe.ngan@gmail.com>
2024-03-04 22:07:53 +0100

    Added memoize function and corresponding unit tests in test_utils.py

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -43,3 +43,29 @@ class TestGetJson(unittest.TestCase):
         with patch('requests.get') as mock_request:
             mock_request.return_value.json.return_value = {"payload": True}
             self.assertEqual(get_json(url), {"payload": True})
+
+
+class TestMemoize(unittest.TestCase):
+    """ Test memoize function
+    """
+    def test_memoize(self):
+        """ Test memoize
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        # Use patch to mock a_method
+        with patch.object(TestClass, 'a_method') as mock_a_method:
+            # Call a_property twice
+            test_class = TestClass()
+            test_class.a_property()
+            test_class.a_property()
+
+            # Assert that a_method was called only once
+            mock_a_method.assert_called_once()
