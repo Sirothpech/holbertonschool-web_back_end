32125af0a704f575e52869d4bcad00cb60caa66d
Sirothpech <christophe.ngan@gmail.com>
2024-03-04 23:15:18 +0100

    Refactor unit tests in test_utils.py for clarity and improved mocking

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,55 +1,60 @@
 #!/usr/bin/env python3
-""" Parameterize a unit test, Mock HTTP calls, Parameterize and patch """
-import unittest
-from unittest.mock import patch
+"""Unittests
+"""
 from parameterized import parameterized
+import unittest
+from unittest.mock import patch, Mock
 from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ TESTCASE """
-    """ to test the function for following inputs """
+    """Test the access_nested_map function
+    """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, answer):
-        """ method to test that the method returns what it is supposed to """
-        self.assertEqual(access_nested_map(nested_map, path), answer)
+    def test_access_nested_map(self, nested_map, path, expected):
+        """Test the access_nested_map function
+        """
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
 
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map_exception(self, nested_map, path):
-        """ method to test that a KeyError is raised properly """
-        with self.assertRaises(KeyError) as error:
+        """Test the access_nested_map function
+        """
+        with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
-        self.assertEqual(error.exception.args[0], path[-1])
 
 
 class TestGetJson(unittest.TestCase):
-    """ TESTCASE """
+    """Test get json function
+    """
     @parameterized.expand([
-        ("http://example.com", {"payload": True}),
-        ("http://holberton.io", {"payload": False}),
+        ("http://example.com",),
+        ("http://holberton.io",),
     ])
-    @patch('test_utils.get_json')
-    def test_get_json(self, test_url, test_payload, mock_get):
-        """ method to test that utils.get_json returns the expected result """
-        mock_get.return_value = test_payload
-        result = get_json(test_url)
-        self.assertEqual(result, test_payload)
+    def test_get_json(self, url):
+        """Test get json function
+        """
+        with patch('requests.get') as mock_request:
+            mock_request.return_value.json.return_value = {"payload": True}
+            self.assertEqual(get_json(url), {"payload": True})
 
 
 class TestMemoize(unittest.TestCase):
-    """ TESTCASE """
+    """Test the memoize function
+    """
     def test_memoize(self):
-        """ Test that when calling a_property twice, the correct result is
-            returned but a_method is only called once using assert_called_once
+        """Test the memoize function
         """
         class TestClass:
+
             def a_method(self):
                 return 42
 
@@ -57,8 +62,8 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, "a_method") as mockMethod:
-            test_class = TestClass()
-            test_class.a_property
-            test_class.a_property
-            mockMethod.assert_called_once
+        with patch.object(TestClass, 'a_method') as mock_method:
+            test = TestClass()
+            test.a_property
+            test.a_property
+            mock_method.assert_called_once()
