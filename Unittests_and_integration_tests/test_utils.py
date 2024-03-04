ead38514174b86f9ef3df0d0e869da1a5cc93b5d
Sirothpech <christophe.ngan@gmail.com>
2024-03-04 22:31:24 +0100

    Refactor variable names in test_utils.py for clarity

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -60,8 +60,8 @@ class TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method') as mock_a_method:
-            test_class = TestClass()
-            test_class.a_property
-            test_class.a_property
-            mock_a_method.assert_called_once()
+        with patch.object(TestClass, 'a_method') as mock_method:
+            test = TestClass()
+            test.a_property
+            test.a_property
+            mock_method.assert_called_once()
