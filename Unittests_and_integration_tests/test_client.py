8d3b0cdee66f348b029f75431708de70edc401d7
Sirothpech <christophe.ngan@gmail.com>
2024-03-05 01:06:00 +0100

    Added unit tests for GithubOrgClient class

---
+++ Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Test client
+"""
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+
+
+class GithubOrgClient(unittest.TestCase):
+    """Test Github
+    """
+    @patch('client.get_json')
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    def test_org(self, org):
+        """Test org method
+        """
+        with patch('client.get_json') as mock_get_json:
+            mock_get_json.return_value = {}
+
+            github_client = GithubOrgClient(org)
+            result = github_client.org
+            mock_get_json.assert_called_once()
+            self.assertEqual(result, {})
