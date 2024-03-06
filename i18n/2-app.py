25c41e651d5a5c966232028f54b57d99828e644a
Sirothpech <christophe.ngan@gmail.com>
2024-03-06 16:54:36 +0100

    Fixed typo in get_locale function in 2-app.py

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -23,7 +23,7 @@ app.config.from_object(Config)
 def get_locale():
     """Get locale
     """
-    return request.accept_langages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
