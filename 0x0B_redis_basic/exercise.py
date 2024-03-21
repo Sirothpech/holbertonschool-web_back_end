8403e06e66be7a40f16db062a37b081edb9c6967
Sirothpech <christophe.ngan@gmail.com>
2024-03-21 10:49:02 +0100

    Add data retrieval and type conversion methods in Redis module

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""0. Writing strings to Redis
+"""1. Reading from Redis and recovering original type
 """
 
-from typing import Union
+from typing import Callable, Optional, Union
 import redis
 import uuid
 
@@ -19,3 +19,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float]:
+        """Get data from redis database"""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, data: bytes) -> str:
+        """Convert bytes to string"""
+        return data.decode('utf-8')
+
+    def get_int(self, data: bytes) -> int:
+        """Convert bytes to int"""
+        return int.from_bytes(data, byteorder='big')
