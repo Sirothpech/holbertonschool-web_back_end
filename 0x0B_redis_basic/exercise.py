3fdda3de42edefa34b4b6fc8235a92fa87b40340
Sirothpech <christophe.ngan@gmail.com>
2024-03-21 10:33:21 +0100

    Added Cache class with store method in exercise.py for Redis data storage

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python3
+"""0. Writing strings to Redis
+"""
+
+from typing import Union
+import redis
+import uuid
+
+
+class Cache:
+    def __init__(self) -> None:
+        """Initialization"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Stores the input data in Redis"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
