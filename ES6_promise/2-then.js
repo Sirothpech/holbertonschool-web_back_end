6c20b6564b769806c0bc189fdfc57e785f310647
Sirothpech <christophe.ngan@gmail.com>
2024-03-22 20:27:06 +0100

    Refactor and fix export of handleResponseFromAPI in 2-then.js

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,8 @@
-function handleResponseFromAPI (promise) {
+export default function handleResponseFromAPI (promise) {
   return promise.then(() => ({
     status: 200,
-    body: 'success'
+    body: 'success',
   }))
     .catch(() => Error())
     .finally(() => console.log('Got a response from the API'));
 }
-
-export default handleResponseFromAPI;
