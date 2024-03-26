3bbf4ea05503806dafe7a95dc4da00e0fbf89857
Sirothpech <christophe.ngan@gmail.com>
2024-03-26 13:36:10 +0100

    Add tests for Index page and improve Cart page tests in api.test.js

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -2,6 +2,22 @@ const chai = require('chai');
 const request = require('request');
 const { expect } = chai;
 
+describe('Index page', () => {
+    const url = 'http://localhost:7865/';
+    it('Checks the status code', (done) => {
+        request(url, (error, response, body) => {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+    it('Checks the body', (done) => {
+        request(url, (error, response, body) => {
+            expect(body).to.equal('Welcome to the payment system');
+            done();
+        });
+    });
+});
+
 describe('Cart page', () => {
     const baseUrl = 'http://localhost:7865';
 
@@ -20,6 +36,15 @@ describe('Cart page', () => {
         const url = `${baseUrl}/cart/${cartId}`;
         request(url, (error, response, body) => {
             expect(response.statusCode).to.equal(404);
+            done();
+        });
+    });
+
+    it('should return 404 status code when cart id is not a number', (done) => {
+        const cartId = 'hello';
+        const url = `${baseUrl}/cart/${cartId}`;
+        request(url, (error, response, body) => {
+            expect(body).to.equal('');
             done();
         });
     });
