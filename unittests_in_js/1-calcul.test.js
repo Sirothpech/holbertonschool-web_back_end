7b193eefe3d75bbc3e4c2ad7ab8f3f200709374c
Sirothpech <christophe.ngan@gmail.com>
2024-03-26 10:53:41 +0100

    Add new test cases for SUBSTRACT and DIVIDE operations in calculateNumber function

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -7,7 +7,9 @@ describe('calculateNumber', () => {
         assert.strictEqual(calculateNumber('SUM', 1.4, -4.5), -3);
         assert.strictEqual(calculateNumber('SUBSTRACT', 1.4, 4.5), -4);
         assert.strictEqual(calculateNumber('SUBSTRACT', 2, 2), 0);
+        assert.strictEqual(calculateNumber('SUBSTRACT', 2.7, 2), 1);
         assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+        assert.strictEqual(calculateNumber('DIVIDE', -3, 1.2), -3);
         assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
     });
 });
