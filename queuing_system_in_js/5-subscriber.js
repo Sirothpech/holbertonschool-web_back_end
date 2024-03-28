b2847d7d8358c77f50a133f42ca2edbcbe34788d
Sirothpech <christophe.ngan@gmail.com>
2024-03-28 12:15:29 +0100

    Add publisher and subscriber scripts for Redis messaging system

---
+++ queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,19 @@
+import { createClient } from "redis";
+
+const client = createClient();
+
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.on('error', () => console.log(`Redis client not connected to the server: ${error}`));
+
+const channel = 'holberton school channel';
+
+client.subscribe(channel);
+
+client.on('message', (channel, message) => {
+    console.log(message);
+    if(message === 'KILL_SERVER') {
+        client.unsubscribe();
+        process.exit();
+    }
+})
