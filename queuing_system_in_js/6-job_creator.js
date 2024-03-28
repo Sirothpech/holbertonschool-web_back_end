aa8b13276043603832f74043fa5076d5888512c3
Sirothpech <christophe.ngan@gmail.com>
2024-03-28 13:41:26 +0100

    Update notification message and phone number in job creator

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -3,8 +3,8 @@ import { createQueue } from 'kue';
 const queue = createQueue();
 
 const data = {
-    phoneNumber: '0649854070',
-    message: 'Hello World!',
+    phoneNumber: '4153518780',
+    message: 'This is the code to verify your account',
 }
 
 const job = queue.create('push_notification_code', data)
